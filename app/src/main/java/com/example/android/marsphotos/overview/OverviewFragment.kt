8ec774fe528a3f84392d7854005a878fc22a0fcf
markjasongalang3 <markjasongalang3@gmail.com>
2023-05-18 10:56:05 +0800

    initial commit

--- app/src/main/java/com/example/android/marsphotos/overview/OverviewFragment.kt
+++ app/src/main/java/com/example/android/marsphotos/overview/OverviewFragment.kt
@@ -23,6 +23,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.android.marsphotos.databinding.FragmentOverviewBinding
+import com.example.android.marsphotos.databinding.GridViewItemBinding
 
 /**
  * This fragment shows the the status of the Mars photos web services transaction.
@@ -46,6 +47,8 @@ class OverviewFragment : Fragment() {
 
         // Giving the binding access to the OverviewViewModel
         binding.viewModel = viewModel
+
+        binding.photosGrid.adapter = PhotoGridAdapter()
 
         return binding.root
     }
